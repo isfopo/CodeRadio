a9dd8ff0a77eaea43f7bce06470ca64ecd3903bf
Isaac Poole <isaac@switcherstudio.com>
2023-05-10 08:48:25 -0400

    gets radio data on open

--- CodeRadio/View/MainPage.xaml.cs
+++ CodeRadio/View/MainPage.xaml.cs
@@ -4,11 +4,20 @@ namespace CodeRadio;
 
 public partial class MainPage : ContentPage
 {
+	MainViewModel viewModel;
+
 	public MainPage(MainViewModel viewModel)
 	{
 		InitializeComponent();
 		BindingContext = viewModel;
+		this.viewModel = viewModel;
 	}
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+		viewModel.GetRadioCommand.Execute(null);
+    }
 }
 
 
