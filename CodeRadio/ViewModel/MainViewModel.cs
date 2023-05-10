c372fcf178b2d0a4b01eeb3323a823ec7e9f3f08
Isaac Poole <isaac@switcherstudio.com>
2023-05-10 08:24:57 -0400

    breaks up res properties

--- CodeRadio/ViewModel/MainViewModel.cs
+++ CodeRadio/ViewModel/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Text.Json.Serialization;
 using CodeRadio.Model;
 using CodeRadio.Services;
 using CommunityToolkit.Mvvm;
@@ -12,8 +13,29 @@ public partial class MainViewModel : BaseViewModel
 {
 	RadioService radioService;
 
-	[ObservableProperty]
-	public RadioResponse response;
+    [ObservableProperty]
+    public Station station;
+
+    [ObservableProperty]
+    public Listeners listeners;
+
+    [ObservableProperty]
+    public Live live;
+
+    [ObservableProperty]
+    public NowPlaying nowPlaying;
+
+    [ObservableProperty]
+    public PlayingNext playingNext;
+
+    [ObservableProperty]
+    public NowPlaying[] songHistory;
+
+    [ObservableProperty]
+    public bool isOnline;
+
+    [ObservableProperty]
+    public string cache;
 
     IConnectivity connectivity;
 
@@ -47,7 +69,15 @@ public partial class MainViewModel : BaseViewModel
 			return;
 		}
 
-		Response = res;
+        Station = res.Station;
+        Listeners = res.Listeners;
+        Live = res.Live;
+        NowPlaying = res.NowPlaying;
+        PlayingNext = res.PlayingNext;
+        SongHistory = res.SongHistory;
+        IsOnline = res.IsOnline;
+        Cache = res.Cache;
+
 		IsBusy = false;
 	}
 }
