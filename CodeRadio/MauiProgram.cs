e56df55978de18d4d6ae4a4af8638e41912ee0ed
Isaac Poole <isaac@switcherstudio.com>
2023-05-21 12:20:01 -0400

    uses font awesome for icon

--- CodeRadio/MauiProgram.cs
+++ CodeRadio/MauiProgram.cs
@@ -17,7 +17,8 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+				fonts.AddFont("FontAwesomeSolid-900.otf", "FontAwesomeSolid");
+            });
 
         builder.Services.AddSingleton<IConnectivity>(Connectivity.Current);
 
